c2b14d10f03a5731ad76b2b203afc08d6d8ccb15
Sverre.Thune <sk.thun05@gmail.com>
2026-01-14 14:52:24 +0100

    Ryddet i"checkoutpage" og flyttet det til egen mappe i react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from 'react-router';
-import { HomePage } from './pages/HomePage';
-import { CheckoutPage } from './pages/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
-import './App.css';
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import "./App.css";
 
 function App() {
   return (
